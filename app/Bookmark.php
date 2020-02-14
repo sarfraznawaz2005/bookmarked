999c7df707e482e41dc6ad3d1fad341be054cdbb
Sarfraz Ahmed <sarfraz@onsupport.com>
2020-02-14 17:44:59 +0500

    update

---
+++ app/Bookmark.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bookmark extends Model
+{
+    public function tags()
+    {
+        return $this->hasMany(Tag::class);
+    }
+}
---
+++ database/migrations/2020_02_14_122438_create_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', static function (Blueprint $table) {
+            $table->bigInteger('bookmark_id')->unsigned()->index();
+            $table->string('tag')->index();
+
+            $table->foreign('bookmark_id')
+                ->references('id')
+                ->on('bookmarks')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
